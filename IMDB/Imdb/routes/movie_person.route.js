fff50ca1d993dc77f27be0740ccc226da30d710a
deepthibalusu17 <deepthibalusu@gmail.com>
2021-02-08 23:01:32 +0530

    flipkart-clone

--- IMDB/Imdb/routes/movie_person.route.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express').Router();
-const controller = require('../controllers/movie_person.controller');
-const {verifyLoginAdmin} = require('../middleware/verifyLogin');
-
-Router.post('/create',verifyLoginAdmin,controller.addMovieToPersonRelation);
-Router.get('/',verifyLoginAdmin,controller.findAllRelations);
-Router.delete('/delete/:id',verifyLoginAdmin,controller.deleteRelation);
-
-module.exports = Router;
--- IMDB/Imdb/routes/wishlist.route.js
+++
@@ -1,8 +0,0 @@
-const Router = require('express').Router();
-const controller = require('../controllers/wishlist.controller');
-const {verifyLoginUser} = require('../middleware/verifyLogin');
-
-Router.patch('/:movieId',verifyLoginUser,controller.addToWishlist);
-Router.get('/',verifyLoginUser,controller.getWishlist);
-Router.delete('/delete/:movieId',verifyLoginUser,controller.deleteFromWishlist);
-module.exports = Router;
